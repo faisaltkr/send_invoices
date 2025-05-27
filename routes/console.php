4e62d755f7b521bfa3ced228b57865539219b4b1
Faisal <faisal@salesfokuz.in>
2025-05-27 11:56:04 +0530

    readme commit

--- routes/console.php
+++ routes/console.php
@@ -11,29 +11,21 @@ use Illuminate\Support\Facades\Schedule;
  * Generate monthly invoice on monthly once 
  */
 
-Schedule::call(function () {
-    Artisan::call('app:generate-invoices');
-})->monthly();
-
-
+ Schedule::command('app:generate-invoices')->monthly(31, '12:00');
 
 /**
  * 
  * Handle if any pdf generation faileds 
  */
-Schedule::call(function () {
-    Artisan::call('app:handle-failed-jobs');
-})->monthlyOn('02:00');
+
+Schedule::command('app:handle-failed-jobs')->monthlyOn(31, '02:00');
 
 /**
  * 
  * only send the pdfs are created that is why scheduled at 4 am monthly once
  */
 
-Schedule::call(function () {
-    Artisan::call('app:trigger-invoice-email');
-})->monthlyOn('04:00');
-
+Schedule::command('app:trigger-invoice-email')->monthlyOn(31, '04:00');
 
 //php artisan queue:work --tries=3
 
